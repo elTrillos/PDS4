6546c63bebde01f8111c97e06be31de2680b7caa
elTrillos <strillos@miuandes.cl>
2022-11-13 18:43:14 -0300

    cambiado trivia

--- telegrambot.py
+++ telegrambot.py
@@ -104,8 +104,12 @@ def bot_send_text(message):
     global stop_iterator
     global loser_list
     global question_count
+    global current_question
+    global next_question
     global trivia_start
     global multiple_question_response
+    next_question=True
+    current_question=0
     markup = ForceReply()
     val_try = 0
     if(message.text.startswith("/")):
@@ -148,8 +152,10 @@ def bot_send_text(message):
                 except:
                     bot.send_message(message.chat.id,"no es un numero, ingrese el numero de intentos")
         #Trivia _-------------------------------------------------------------------_
-        elif(trivia_start == True and question_count == 0 ):
+        elif(trivia_start == True and question_count == 0 and next_question==True):
             number_start = False
+            print("xd")
+            print(next_question)
             if(stop_iterator != True):
                 if(message.text == 'stop'):
                     stop_iterator = True
@@ -169,49 +175,59 @@ def bot_send_text(message):
                 bot.send_message(message.chat.id,"linea 162 {}".format(question_count))
                 try:
                     question_count = int(message.text)
+                    
+                    next_question=False
                     bot.send_message(message.chat.id,"cantidad de preguntas {}".format(question_count))
                     if(question_count > 0):
-                        for i in range(question_count):
-                            response_list = []
-                            response_list.append(random_categories[i]['incorrect_answers'][0])
-                            response_list.append(random_categories[i]['incorrect_answers'][1])
-                            response_list.append(random_categories[i]['incorrect_answers'][2])
-                            response_list.append(random_categories[i]['correct_answer'])
-                            random.shuffle(response_list)
-                            bot.send_message(message.chat.id, "tema: {}\n pregunta n°{}: {} \n alternativas: \nA: {}\nB: {}\nC: {}\nD: {}".format(
-                                random_categories[i]['category'],i+1,random_categories[i]['question'],response_list[0],
-                                response_list[1],response_list[2],response_list[3]))
-                            multiple_question_response.append(response_list)
-                            response_list = []
+                        response_list = []
+                        response_list.append(random_categories[current_question]['incorrect_answers'][0])
+                        response_list.append(random_categories[current_question]['incorrect_answers'][1])
+                        response_list.append(random_categories[current_question]['incorrect_answers'][2])
+                        response_list.append(random_categories[current_question]['correct_answer'])
+                        random.shuffle(response_list)
+                        bot.send_message(message.chat.id, "tema: {}\n pregunta n°{}: {} \n alternativas: \nA: {}\nB: {}\nC: {}\nD: {}".format(
+                            random_categories[current_question]['category'],current_question+1,random_categories[current_question]['question'],response_list[0],
+                            response_list[1],response_list[2],response_list[3]))
+                        multiple_question_response.append(response_list)
+                        response_list = []
                     else:
                         bot.send_message(message.chat.id, "la cantidad de preguntas es invalida")
                         question_count = 0
                 except:
                     bot.send_message(message.chat.id,"no es un numero, ingrese la cantidad (numero) de pregunta")
-        elif(trivia_start == True and question_count != 0):
+        elif(trivia_start == True and question_count != 0 and next_question==False):
             if(message.text == 'A' or message.text == 'a'):
                 if(multiple_question_response[0][0] == random_categories[0]['correct_answer']):
                     bot.send_message(message.chat.id,"esta correcta ")
+                    current_question+=1
+                    next_question=True
                 else:
                     bot.send_message(message.chat.id,"esta INcorrecta ")
             if(message.text == 'B' or message.text == 'b'):
                 if(multiple_question_response[0][1] == random_categories[0]['correct_answer']):
                     bot.send_message(message.chat.id,"esta correcta ")
+                    current_question+=1
+                    next_question=True
                 else:
                     bot.send_message(message.chat.id,"esta INcorrecta ")
             if(message.text == 'C' or message.text == 'c'):
                 if(multiple_question_response[0][2] == random_categories[0]['correct_answer']):
                     bot.send_message(message.chat.id,"esta correcta ")
+                    current_question+=1
+                    next_question=True
                 else:
                     bot.send_message(message.chat.id,"esta INcorrecta ")
             if(message.text == 'D' or message.text == 'd'):
                 if(multiple_question_response[0][3] == random_categories[0]['correct_answer']):
                     bot.send_message(message.chat.id,"esta correcta ")
+                    current_question+=1
+                    next_question=True
                 else:
                     bot.send_message(message.chat.id,"esta INcorrecta ")
         #-----------------------------------------------------------------------------
         elif(number_start == True and max_number != 0 and (message.json)['from']['first_name'] in user_first_game):
             try:
+                print("xdddd")
                 for i in range(len(trys_list)):
                     if(trys_list[i][0] == (message.json)['from']['first_name']):
                         bot.send_message(message.chat.id,"intento n°{} de {}. escrito por {} es...".format(trys_list[i][1],trys,trys_list[i][0]))
